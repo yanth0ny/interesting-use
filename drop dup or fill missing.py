34d8bed4c636ffbaebfd9323f516e87d13454877
yanth0ny <53915150+yanth0ny@users.noreply.github.com>
2019-08-27 20:12:30 +0100

    Create drop dup or fill missing.py

---
+++ drop dup or fill missing.py
@@ -0,0 +1,20 @@
+# Create the new DataFrame: tracks
+tracks = billboard[['year', 'artist', 'track', 'time']]
+
+# Print info of tracks
+print(tracks.info())
+
+# Drop the duplicates: tracks_no_duplicates
+tracks_no_duplicates = tracks.drop_duplicates()
+
+# Print info of tracks
+print(tracks_no_duplicates.info())
+
+# Calculate the mean of the Ozone column: oz_mean
+oz_mean = airquality.Ozone.mean()
+
+# Replace all the missing values in the Ozone column with the mean
+airquality['Ozone'] = airquality.Ozone.fillna(oz_mean)
+
+# Print the info of airquality
+print(airquality.info())
