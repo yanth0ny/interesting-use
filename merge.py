d76203f6edd6386331fd3a8b8960ff1752234d85
yanth0ny <53915150+yanth0ny@users.noreply.github.com>
2019-08-27 20:08:22 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -3,3 +3,20 @@ o2o = pd.merge(left=site, right=visited, left_on='name', right_on='site')
 
 # Print o2o
 print(o2o)
+
+
+# Merge the DataFrames: m2o
+m2o = pd.merge(left=site, right=visited, left_on='name', right_on='site')
+
+# Print m2o
+print(m2o)
+
+
+# Merge site and visited: m2m
+m2m = pd.merge(left=site, right=visited, left_on='name', right_on='site')
+
+# Merge m2m and survey: m2m
+m2m = pd.merge(left=m2m, right=survey, left_on='ident', right_on='taken')
+
+# Print the first 20 lines of m2m
+print(m2m.head(20))
