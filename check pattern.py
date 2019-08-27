96a97ab2118a8f78a51a4dad0db9eea82f48adf9
yanth0ny <53915150+yanth0ny@users.noreply.github.com>
2019-08-27 20:10:49 +0100

    Update check pattern.py

--- check pattern.py
+++ check pattern.py
@@ -11,3 +11,13 @@ print(bool(result))
 # See if the pattern matches
 result2 = prog.match('1123-456-7890')
 print(bool(result2))
+
+
+# Import the regular expression module
+import re
+
+# Find the numeric values: matches
+matches = re.findall('\d+', 'the recipe calls for 10 strawberries and 1 banana')
+
+# Print the matches
+print(matches)
