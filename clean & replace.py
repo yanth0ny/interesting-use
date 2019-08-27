5e4640883961d77862aaf7e948aefc544e8814a2
yanth0ny <53915150+yanth0ny@users.noreply.github.com>
2019-08-27 20:17:37 +0100

    Update clean & replace.py

--- clean & replace.py
+++ clean & replace.py
@@ -27,3 +27,16 @@ tips['total_dollar_re'] = tips.total_dollar.apply(lambda x: re.findall('\d+\.\d+
 
 # Print the head of tips
 print(tips.head())
+
+
+# Assert that country does not contain any missing values
+assert pd.notnull(gapminder.country).all()
+
+# Assert that year does not contain any missing values
+assert pd.notnull(gapminder.year).all()
+
+# Drop the missing values
+gapminder = gapminder.dropna(axis=0, how='any')
+
+# Print the shape of gapminder
+print(gapminder.shape)
