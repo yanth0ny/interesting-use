8e74ef64d96244bb779746a11b4ecc2fb71cf7b4
yanth0ny <53915150+yanth0ny@users.noreply.github.com>
2019-08-27 20:12:57 +0100

    Update drop dup or fill missing.py

--- drop dup or fill missing.py
+++ drop dup or fill missing.py
@@ -18,3 +18,11 @@ airquality['Ozone'] = airquality.Ozone.fillna(oz_mean)
 
 # Print the info of airquality
 print(airquality.info())
+
+
+#TEST
+# Assert that there are no missing values
+assert ebola.notnull().all().all()
+
+# Assert that all values are >= 0
+assert (ebola >= 0).all().all()
