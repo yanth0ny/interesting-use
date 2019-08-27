c1aba9b22ec431fc050fde02d6019488c76bacdc
yanth0ny <53915150+yanth0ny@users.noreply.github.com>
2019-08-27 20:11:51 +0100

    Create clean & replace.py

---
+++ clean & replace.py
@@ -0,0 +1,29 @@
+# Define recode_gender()
+def recode_gender(gender):
+
+    # Return 0 if gender is 'Female'
+    if gender == 'Female':
+        return 0
+    
+    # Return 1 if gender is 'Male'    
+    elif gender == 'Male':
+        return 1
+    
+    # Return np.nan    
+    else:
+        return np.nan
+
+# Apply the function to the sex column
+tips['recode'] = tips.sex.apply(recode_gender)
+
+# Print the first five rows of tips
+print(tips.head())
+
+# Write the lambda function using replace
+tips['total_dollar_replace'] = tips.total_dollar.apply(lambda x: x.replace('$', ''))
+
+# Write the lambda function using regular expressions
+tips['total_dollar_re'] = tips.total_dollar.apply(lambda x: re.findall('\d+\.\d+', x)[0])
+
+# Print the head of tips
+print(tips.head())
